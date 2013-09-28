6cc454773e3d006f491a37f4106bfa20c07c0bb1
Danielle Levi <dslevi12@gmail.com>
2013-09-28 14:52:09 -0700

    Condensed code, again

--- file_sorter.py
+++ file_sorter.py
@@ -1,6 +1,6 @@
 """
 Before using file_sorter.py,
-place files to be sorted into a sub-dir titled "UnsortedFiles"
+place files to be sorted into sub-dir titled "UnsortedFiles"
 
 file_sorter.py creates 26 directories, one for each letter of the alphabet,
 in a dir called "SortedFiles".
@@ -10,30 +10,28 @@ Loops through files in "UnsortedFiles" and places each into the corresponding al
 
 import os, shutil
 
-current_path = os.getcwd()
-unsorted_path = current_path + "/UnsortedFiles"
-sorted_path = current_path + "/SortedFiles"
+unsorted_path = os.getcwd() + "/UnsortedFiles"
+sorted_path = os.getcwd() + "/SortedFiles"
 
-#removes SortedFiles dir if it already exists (does not work if full...)
-#if os.path.exists(sorted_path):
-#   os.removedirs(sorted_path)
+#removes SortedFiles dir if it already exists (does not work...)
+# if os.path.exists(sorted_path):
+#   for root, dirs, files in os.walk(sorted_path, topdown=False):
+#     for name in files:
+#         os.remove(os.path.join(root, name))
+#     for name in dirs:
+#         os.rmdir(os.path.join(root, name))
 
-#creates SortedFiled dir and cds into it
-os.mkdir("./SortedFiles")
-os.chdir(sorted_path)
+#creates SortedFiled dir
+os.mkdir(sorted_path)
 
 #Creates a sub-folder for each letter in alphabet in SortedFiles
 for x in range(26):
-    os.mkdir(chr(x + 97))
+    os.mkdir("SortedFiles/" + chr(x + 97))
 
 #Cycles through list of names of unsorted files and places them alphabetically in appropriate sub-directory
-unsorted_files = os.listdir(unsorted_path)
-
-for name in unsorted_files:
-    first_letter = name[0]
-    if os.path.exists(sorted_path + "/" + first_letter):
-        shutil.move(unsorted_path + "/" + name, sorted_path + "/" + first_letter)
-
+for name in os.listdir(unsorted_path):
+    if os.path.exists(sorted_path + "/" + name[0]):
+        shutil.move(unsorted_path + "/" + name, sorted_path + "/" + name[0])
 
 
 
